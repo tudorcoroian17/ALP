882da79dcc6c77b99728db4287232ce1c3ba8844
Tudor Coroian <tudorcoroian17w@gmail.com>
2023-05-27 10:28:57 +0300

    Add data segment for G2 lab exam

---
+++ LabExam/G2/dataseg.asm
@@ -0,0 +1,6 @@
+DATA SEGMENT PARA PUBLIC 'DATA'
+CHORUS  DB  "We don't talk about Bruno, no, no, no!", 0AH, 0DH, "We don't talk about Bruno... but", 0AH, 0DH, "$"
+VERSE1  DB  "It was my wedding day", 0AH, 0DH, "It was our wedding day", 0AH, 0DH, "We were getting ready, and there wasn't a cloud in the sky", 0AH, 0DH, "No clouds allowed in the sky", 0AH, 0DH, "$"
+VERSE2  DB  "Bruno walks in with a mischievous grin-", 0AH, 0DH, "Thunder!", 0AH, 0DH, "You telling this story, or am I?", 0AH, 0DH, "I'm sorry, mi vida, go on", 0AH, 0DH, "$"
+VERSE3  DB  "Bruno says, 'It looks like rain'", 0AH, 0DH, "Why did he tell us?", 0AH, 0DH, "In doing so, he floods my brain", 0AH, 0DH, "Abuela, get the umbrellas", 0AH, 0DH, "Married in a hurricane", 0AH, 0DH, "What a joyous day... but anyway", 0AH, 0DH, "$"
+DATA ENDS
