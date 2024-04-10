ff7286a2a388dad4fd9f6d0620ba5c108d2f750a
Tudor Coroian <tudorcoroian17w@gmail.com>
2024-04-10 10:08:03 +0300

    Modify file

--- Lab_07/hw7e3.asm
+++ Lab_07/hw7e3.asm
@@ -2,6 +2,8 @@ DATA SEGMENT PARA PUBLIC 'DATA'
     VEC     DB  'we dont talk about Bruno$'
     ; This is a $-terminated string. You can test for it
     ; to see if you reached the end of the string.
+    VOW     DB  'o'
+    SYM     DB  '*'
 DATA ENDS
 
 CODE SEGMENT PARA PUBLIC 'CODE'
@@ -15,8 +17,10 @@ MOV AX, DATA
 MOV DS, AX
 ; your code starts here
 PUSH OFFSET VEC
+PUSH OFFSET VOW
+PUSH OFFSET SYM
 CALL REPLACE_VOWELS
-ADD SP,2
+ADD SP,6
 ; your code ends here
 RET
 START ENDP
